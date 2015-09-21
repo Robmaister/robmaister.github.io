4d3f347ae2d5ba6d089ececcc7cea0201e572a5e
Robert Rouhani <robert.rouhani@gmail.com>
2015-09-21 18:04:53 -0400

    Jekyll-ified site, added history.js

---
+++ event-planning.md
@@ -0,0 +1,4 @@
+---
+layout: page
+title: Event Planning
+---
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var background_images = [
-	'ZyzTf6b.jpg',
+	/*'ZyzTf6b.jpg',
 	'2010-11-26_13.40.30.png',
-	'DIV7gaC.png',
+	'DIV7gaC.png',*/
 	'jp_diner_port0037.jpg',
 	'TopHat-2013-07-04-00.03.45.0008.png'
 ];
@@ -9,26 +9,59 @@ var background_images = [
 var background_index = 0;
 
 function transition_background() {
-	$('body').css("background-image", "url(../img/" + background_images[background_index] + ")");
+	$('body').css("background-image", "url(/img/" + background_images[background_index] + ")");
 	background_index++;
 	background_index = background_index % background_images.length;
 }
 
 $(document).ready(function () {
 
+    //HACK swap backgrounds to test
 	transition_background();
 	setInterval(function() {
 		transition_background();
 	}, 10000);
-	
 
-	$(".nav-item").click(function () {
-		
+    //Collapse sidebar if click on anything else while expanded
+    $(document).click(function(e) {
+        var target = e.target;
+        if ($(".header-container").hasClass("expanded") && 
+            !$(target).is(".header-container") &&
+            !$(target).parents().is(".header-container")) {
+            
+            $(".header-container").removeClass("expanded");
+        }
+    });
+
+    //Handle links dynamically
+	$(".nav-item").click(function(e) {
+        e.preventDefault();
+		History.pushState(null, document.title, $(this).children("a:first").attr('href'));
 	});
+    $(".nav-title").click(function(e) {
+        e.preventDefault();
+        History.pushState(null, document.title, $(this).attr('href'));
+    });
     
+    //Toggle sidebar when the hamburger icon is clicked
     $("#nav-toggle").click(function () {
         if ($(this).is(":visible")) {
             $(".header-container").toggleClass("expanded");
         }
+    });
+    
+    //Deal with history
+    History.Adapter.bind(window, "statechange", function() {
+        var state = History.getState();
+        var data = state.data;
+        
+        $("#main-content").addClass("loading");
+        setTimeout(function() {
+            $("#main-content").load(state.url + " #main-content>*", function() {
+                setTimeout(function( ){
+                    $("#main-content").removeClass("loading");
+                }, 10);
+            });
+        }, 500);
     });
 });
